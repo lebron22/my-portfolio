ffff6a800e269ea6ad52edf14c0f8e3008837ecb
Kamil Bronisz <kbronisz8@gmail.com>
2019-09-03 09:24:40 +0200

    Add a view handling loading DOM content

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
+//hide loading page
+const loading = document.querySelector(".loading");
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    loading.classList.add("hidden");
+  }, 300);
+});
+
 //hide navigation after click in navigation link
 
 const navLinks = [...document.querySelectorAll(".navigation__link")];
@@ -11,7 +19,7 @@ navLinks.forEach(link => {
   link.addEventListener("click", () => handleMenuToggle());
 });
 
-//smooth scroll
+//smooth scroll with jQuery
 $('a[href*="#"]').on("click", function(e) {
   e.preventDefault();
 
